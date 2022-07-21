03527b5156b1e4807132a7b2d020f6615429c5c8
杜康 <banshee1115@163.com>
2022-07-21 19:20:13 +0800

    commit - 与juice-end联调第一个接口

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,31 @@
 <!--
  * @LastEditors: 杜康
- * @LastEditTime: 2022-07-21 17:02:21
+ * @LastEditTime: 2022-07-21 19:17:46
 -->
 <script lang="ts" setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
+import axios from 'axios'
 const form = reactive({
   account: '',
   password: ''
+})
+const welcomeWords = ref('')
+const mounted = onMounted(() => {
+  axios({
+    method: 'get',
+    url: '/juice-end/cats',
+  })
+    .then((res: any) => {
+      welcomeWords.value = res.data
+    })
 })
 </script>
 
 <template>
   <section class="w-full h-full flex justify-center items-center bg-gray-50">
-    <div
-      class="w-96 h-96 rounded-xl shadow-xl p-4 border flex flex-col justify-center items-center space-y-24 bg-white">
+    <div class="w-96 h-96 rounded-xl shadow-xl p-4 border flex flex-col justify-center items-center space-y-8 bg-white">
       <a-avatar :size="64">Juice</a-avatar>
+      <div>{{ welcomeWords }}</div>
       <a-from class="w-full">
         <a-form-item field="account" label="账户">
           <a-input v-model="form.account" placeholder="请输入账户" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /*
  * @LastEditors: 杜康
- * @LastEditTime: 2022-07-21 14:39:35
+ * @LastEditTime: 2022-07-21 19:15:01
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -13,6 +13,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve('./src/')
+    }
+  },
+  server: {
+    proxy: {
+      '/juice-end': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/juice\-end/, '')
+      }
     }
   }
 })
