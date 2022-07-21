22a8623514d8101ce600ea3ec086ebe09d29bc47
杜康 <banshee1115@163.com>
2022-07-21 09:00:40 +0800

    commit - 添加normalize.css ， 添加tailwind.css

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+/*
+ * @LastEditors: 杜康
+ * @LastEditTime: 2022-07-21 08:58:39
+ */
 import { createApp } from 'vue'
-import './style.css'
+import 'normalize.css/normalize.css';
+import './main.css'
+// import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/*
+ * @LastEditors: 杜康
+ * @LastEditTime: 2022-07-20 18:24:41
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
