311e46e67b199bb91c376c8a8b1a5139f98a2294
杜康 <banshee1115@163.com>
2022-08-12 14:52:22 +0800

    commit - 项目文件处理

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,11 @@
 <!--
  * @LastEditors: 杜康
- * @LastEditTime: 2022-08-12 14:47:22
+ * @LastEditTime: 2022-08-12 14:52:02
 -->
 <script lang="ts" setup>
-import Vue from "vue";
+// import Vue from "vue";
 </script>
 
 <template>
-this is home
+<!-- this is home -->
 </template>
